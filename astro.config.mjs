a7c706a0fa0567f882e789fc81583617b5ef9ea3
WingChunWong <huangrongjun2200@outlook.com>
2026-03-04 18:47:48 +0800

    更新配置文件，添加站点和基础路径；修正图标链接以使用动态基础路径

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
+	site: "https://lanrhyme.github.io",
+	base: "/Website-MicYou",
 	i18n: {
 		locales: ["en", "zh"],
 		defaultLocale: "zh",
